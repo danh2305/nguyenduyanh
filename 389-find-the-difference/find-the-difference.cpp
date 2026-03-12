7f92438c9fba62f6b72d4b00df26653a08b9fafb
Nguyễn Duy Anh <165299631+danh2305@users.noreply.github.com>
2026-03-13 01:12:52 +0700

    Time: 0 ms (100.00%) | Memory: 9.2 MB (49.88%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char result = 0;
-
-        for(char c : s)
-            result ^= c;
-
-        for(char c : t)
-            result ^= c;
-
+        char result = {};
+        for(char c : s) result ^= c;
+        for(char c : t) result ^=c;
         return result;
     }
 };
